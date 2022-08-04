ea49922aaa1cf6cb8988f5b8644df1a892852bf7
Rawaz Ali <rawazali988@gmail.com>
2022-08-04 23:00:17 +0300

    sign up page added

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ export default function HeroSection() {
     <div className="relative flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0">
       <div className="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0">
         <img
-          className="object-cover w-full h-56 rounded shadow-xl lg:rounded-none lg:shadow-none md:h-96 lg:h-full"
+          className="object-cover w-full h-56 lg:rounded-none lg:shadow-none md:h-96 lg:h-full"
           src={require("../images/heroimage.png")}
           alt="shopping"
         />
@@ -17,30 +17,28 @@ export default function HeroSection() {
             Welcome to Phoenix Store
           </p>
           <h2 className="mb-6 font-sans text-3xl font-bold tracking-tight text-custom1 sm:text-4xl sm:leading-none">
-            Anything you Want
-            <br className="hidden md:block" />
-            We Have it Here ...
+            Anything you Want <br/> We Have it Here ...
           </h2>
           <p className="pr-5 text-base text-custom1 md:text-lg">
             Our store has best products in each category :
           </p>
           <p className="pr-5 mb-5 text-base text-custom2 md:text-lg">
-            Electronics , Men Clothes , Women Clothes , Sport Clothes
+            Electronics , Men Clothes , Women Clothes <br/> Sport Clothes
           </p>
           <div className="flex items-center">
             <Link
               to="/products"
-              className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide text-custom1 bg-custom4 transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+              className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide text-custom1 bg-custom4 transition duration-200 rounded shadow-lg hover:bg-custom3 focus:shadow-outline focus:outline-none"
             >
               Shop Now
             </Link>
-            <a
-              href="/"
+            <Link
+              to='/login'
               aria-label=""
-              className="inline-flex items-center font-semibold text-custom1 transition-colors duration-200 hover:text-deep-purple-accent-700"
+              className="inline-flex items-center font-semibold text-custom1 hover:text-slate-500"
             >
-              Learn more
-            </a>
+              Login first
+            </Link>
           </div>
         </div>
       </div>
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,159 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { login } from "../app/slices/authenticationSlice";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export default function Signup() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [form, setForm] = useState({});
+
+  function submitHandler(e) {
+    e.preventDefault();
+    dispatch(login(form.username));
+    navigate("/", { replace: true }); 
+  }
+
+  function onChangeHandler(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    setForm((values) => ({ ...values, [name]: value }));
+    
+
+   
+  }
   return (
-    <div>Signup</div>
-  )
+    <section id="signup" className="w-full h-full bg-slate-100 ">
+      <div className="flex justify-center">
+        <div className="flex items-center justify-center mt-12 h-full">
+          <div className=" flex flex-col sm:w-full w-4/5 px-6 py-1 pt-0 mb-6 shadow-lg rounded-3xl bg-zinc-50 border-0">
+            <div className="text-center my-3 font-bold">
+              <h1 className="text-custom2 text-xl">Sign up Phoenix Store</h1>
+              <hr className="sm:mt-2 border-amber-700" />
+            </div>
+            <form onSubmit={submitHandler}>
+              <div className="relative w-full mb-3">
+                <label
+                  className="block uppercase text-custom5  text-xs font-bold mb-1"
+                  htmlFor="username"
+                >
+                  Username
+                </label>
+                <input
+                  type="text"
+                  className="border-0 px-3 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
+                  placeholder="username"
+                  style={{ transition: "all .15s ease" }}
+                  name="username"
+                  onChange={onChangeHandler}
+                  value={form.username || ''}
+                />
+              </div>
+              <div className="relative w-full mb-3">
+                <label
+                  className="block uppercase text-custom5  text-xs font-bold mb-1"
+                  htmlFor="username"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="border-0 px-3 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
+                  placeholder="Email"
+                  style={{ transition: "all .15s ease" }}
+                  name="email"
+                 
+                />
+              </div>
+
+              <div className="relative w-full mb-3">
+                <label
+                  className="block uppercase text-custom5  text-xs font-bold mb-1"
+                  htmlFor="password"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="border-0 px-3 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
+                  placeholder="Password"
+                  style={{ transition: "all .15s ease" }}
+                  name='password'
+                  value={form.password || ''}
+                  onChange={onChangeHandler}
+                />
+              </div>
+              <div>
+                <label className="flex items-center cursor-pointer">
+                  <input
+                    id="customCheckLogin"
+                    type="checkbox"
+                    className="form-checkbox border-0 rounded text-custom5  ml-1 w-5 h-5"
+                    style={{ transition: "all .15s ease" }}
+                  />
+                  <span className="ml-2 text-sm font-semibold text-custom5">
+                    Remember me
+                  </span>
+                </label>
+              </div>
+
+              <div className="flex justify-center mt-2  text-xs underline">
+                
+                <div className=" my-2">
+                  <Link to="/login" className="text-gray-900">
+                    Already Have an account?
+                     Sign In
+                  </Link>
+                </div>
+              </div>
+
+              <div className="text-center mt-4">
+                <button
+                  className="bg-cyan-400 text-white active:bg-white active:text-  text-md font-bold uppercase px-6 py-3 rounded-xl shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
+                  type="submit"
+                  style={{ transition: "all .15s ease" }}
+                >
+                  sign up
+                </button>
+              </div>
+            </form>
+            <div className="px-6 py-4">
+              <div className="text-center mb-3">
+                <h6 className="text-gray-600 text-sm font-bold">
+                  Or Sign in with
+                </h6>
+              </div>
+              <div className="text-center flex flex-wrap ">
+                <button
+                  className="bg-white active:bg-gray-100 text-gray-800 p-2 rounded-lg outline-none focus:outline-none mr-2 mb-1  shadow hover:shadow-lg inline-flex items-center font-bold text-xs"
+                  type="button"
+                  style={{ transition: "all .15s ease" }}
+                >
+                  <img
+                    alt="..."
+                    className="w-5 mr-1"
+                    src={require("../images/icons8-facebook.svg").default}
+                  />
+                  Facebook
+                </button>
+                <button
+                  className="bg-white active:bg-gray-100 text-gray-800 px-4 py-2 rounded-lg outline-none focus:outline-none mr-1 mb-1  shadow hover:shadow-lg inline-flex items-center font-bold text-xs"
+                  type="button"
+                  style={{ transition: "all .15s ease" }}
+                >
+                  <img
+                    alt="..."
+                    className="w-5 mr-1"
+                    src={require("../images/google.svg").default}
+                  />
+                  Google
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
