1366bba2db2e9175e0f42dae1367b49fa08050b0
Rawaz Ali <rawazali988@gmail.com>
2022-08-15 16:29:25 +0300

    men and women page products added

--- src/pages/Women.js
+++ src/pages/Women.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ProductCard from "../components/ProductCard";
 import Layout from "../Layouts/MainLayout";
 
 export default function Women() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [products, setProducts] = useState();
+
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products/category/women's clothing")
+      .then((response) => {
+        console.log("list of products:", response.data);
+        setProducts(response.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <Layout>loading....</Layout>;
+  }
   return (
-    <Layout>Women</Layout>
-  )
+    <Layout>
+      <h1 className="text-md sm:text-3xl mx-10 text-center text-custom1 font-semibold my-4 ">
+        Men
+      </h1>
+      <div className="container  mx-auto p-10 grid grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4  gap-10 object-fill">
+        {products.map((product) => {
+          return <ProductCard product={product} />;
+        })}
+      </div>
+    </Layout>
+  );
 }
